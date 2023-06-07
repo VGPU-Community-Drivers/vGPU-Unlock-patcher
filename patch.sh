9f2925aeefcd5afc8f73327835f6c2f3dfe51325
snowman <snowman@localhost.localdomain>
2023-06-07 14:51:13 +0200

    add compat patch for vgpu vfio module
    this is needed when compiling 525.105 vgpu kvm for nvidia 535.43

--- patch.sh
+++ patch.sh
@@ -529,6 +529,7 @@ if $DO_VGPU; then
         applypatch ${TARGET} vgpu-kvm-kernel-6.1-compat.patch
         applypatch ${TARGET} vgpu-kvm-kernel-6.2-compat.patch
     fi
+    applypatch ${TARGET} vgpu-kvm-nvidia-535.43-compat.patch
     applypatch ${TARGET} workaround-for-cards-with-inforom-error.patch
     applypatch ${TARGET} vcfg-testing.patch
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1E84 0x0000	# RTX 2070 super 8GB
