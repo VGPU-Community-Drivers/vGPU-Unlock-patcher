429698de7e9a986fa14ad7b1177a1fdf94d27740
snowman <snowman@localhost.localdomain>
2023-06-08 19:45:09 +0200

    add support for 474.30 windows guest driver
    added to be consistent with other branches, not compatible
    with nsigpatch adapted for 535.43 similarly as other versions

--- patch.sh
+++ patch.sh
@@ -5,6 +5,7 @@ BASEDIR=$(dirname $0)
 GNRL="NVIDIA-Linux-x86_64-535.43.02"
 VGPU="NVIDIA-Linux-x86_64-535.43.02-vgpu-kvm"
 GRID="NVIDIA-Linux-x86_64-535.43.02-grid"
+#WSYS="NVIDIA-Windows-x86_64-474.30"
 #WSYS="NVIDIA-Windows-x86_64-512.15"
 #WSYS="NVIDIA-Windows-x86_64-516.25"
 #WSYS="NVIDIA-Windows-x86_64-516.59"
